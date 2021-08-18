43cf624dc1743584ce391c1b8acc27c1821b3e2b
Ajit Honnungar <ajithonnungar@Ajits-iMac.local>
2021-08-18 18:17:28 +0530

    first ex on my own

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,11 @@
 # bounce.py
 #
 # Exercise 1.5
+ball_ht = 100.0 #metres
+bounce_ht = 0.6 #is it floating
+
+for i in range(1,11):
+    ball_ht = round(ball_ht * bounce_ht, 4)
+    print (i,ball_ht)
+
+    
