d04db5daa71c7c7c3dbb13a9e9335e820d5144ad
Ajit Honnungar <ajithonnungar@Ajits-iMac.local>
2021-08-23 21:02:26 +0530

    completed exercise2.27

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,14 @@
 # pcost.py
 #
 # Exercise 1.27
+Total = 0
+with open('Data/portfolio.csv','rt') as f:
+    header = next(f)
+    for line in f:
+        linelist = line.split(',')
+        ShareValue = linelist[2].split('\n')[0]
+        intValue = float(ShareValue)
+        NumShares = float(linelist[1])
+        Total = Total + intValue*NumShares
+        
+print('Total cost = ',Total)
