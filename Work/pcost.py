2903ce7f192cf750d55058c9dc46269a09c535f1
Ajit Honnungar <ajithonnungar@Ajits-iMac.local>
2021-08-25 16:09:52 +0530

    remove header var for first next

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,14 +1,23 @@
 # pcost.py
 #
 # Exercise 1.27
-Total = 0
-with open('Data/portfolio.csv','rt') as f:
-    header = next(f)
-    for line in f:
-        linelist = line.split(',')
-        ShareValue = linelist[2].split('\n')[0]
-        intValue = float(ShareValue)
-        NumShares = float(linelist[1])
-        Total = Total + intValue*NumShares
-        
-print('Total cost = ',Total)
+import sys
+def portfolio_cost(filename):
+    Total = 0
+    with open(filename,'rt') as f:
+        next(f)
+        for line in f:
+            linelist = line.split(',')
+            ShareValue = linelist[2].split('\n')[0]
+            intValue = float(ShareValue)
+            NumShares = float(linelist[1])
+            Total = Total + intValue*NumShares
+    return Total
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+    
+cost = portfolio_cost(filename)
+print('Total cost = ', cost)
